25df3e5e494f2035f361216eb5855478b0fbbe61
Mael Lebrun <malebrun@2F5.42angouleme.fr>
2026-05-23 17:19:45 +0200

    fix des trucs

--- player.gd
+++ player.gd
@@ -6,21 +6,28 @@ var mouse_pos:Vector2
 @export var SPEED:float = 5.0
 const JUMP_VELOCITY = 4.5
 @onready var animSprite:AnimatedSprite3D = $AnimatedSprite3D
+var canrotate:bool = true;
+var angle_cible:float = 0.0;
+@onready var CameraCoolDown:Timer = $CameraRotationCooldown
 
-
+func handle_rotation(angle:String) -> void :
+	if angle == 'R':
+		angle_cible += deg_to_rad(45)
+	if angle == 'L':
+		angle_cible += deg_to_rad(-45)
+		
 func _process(delta: float) -> void:
 	mouse_pos = get_viewport().get_mouse_position()
 	torch_dir = get_viewport().get_mouse_position().angle() * 2
 
 	torch_dir = get_viewport().get_mouse_position().angle()
 	torch_light.rotation.y = torch_dir
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 #
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 	
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	
@@ -38,4 +45,17 @@ func _process(delta: float) -> void:
 		animSprite.play("idle")
 		velocity.z = 0
 		velocity.x = 0
+	if (Input.is_action_pressed("rotateleft") && canrotate):
+		canrotate = false;
+		CameraCoolDown.start()
+		handle_rotation('L')
+	if (Input.is_action_pressed("rotateright") && canrotate):
+		canrotate = false;
+		CameraCoolDown.start()
+		handle_rotation('R')
+	rotation.y = lerp_angle(rotation.y, angle_cible, 10 * delta)
 	move_and_slide()
+
+
+func _on_camera_rotation_cooldown_timeout() -> void:
+	canrotate = true
