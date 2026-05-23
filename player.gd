11424b644bf675330eee37d13dd5ec3838fa02ed
lenyperis@hotmail.fr <lenyperis@hotmail.fr>
2026-05-23 15:01:35 +0200

    Fix de sese mort

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 var torch_dir:float
 var mouse_pos:Vector2
-@onready var torch_light = $CollisionShape3D/flash_light
+@onready var torch_light = $flash_light
 @export var SPEED:float = 5.0
 const JUMP_VELOCITY = 4.5
 @onready var animSprite:AnimatedSprite3D = $AnimatedSprite3D
@@ -13,16 +13,16 @@ func _process(delta: float) -> void:
 	torch_dir = get_viewport().get_mouse_position().angle()
 	torch_dir = torch_dir * torch_dir
 
-	#torch_dir = get_viewport().get_mouse_position().angle()
+	torch_dir = get_viewport().get_mouse_position().angle()
 	torch_light.rotation.y = torch_dir
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 #
 	## Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	#
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+	
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	
 	var direction:Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
