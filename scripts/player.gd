041d6a32fc9fbd57528d2740659afe52e883f2ae
Leny Peris <lenyperis@hotmail.fr>
2026-05-24 16:17:47 +0200

    merge

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,6 +13,7 @@ var canrotate:bool = true;
 var angle_cible:float = 0.0;
 @onready var CameraCoolDown:Timer = $CameraRotationCooldown
 @onready var blinkTimer:Timer = $TorchBlinking
+@onready var ventouse_preload = preload("res://scenes/Player/atk/ventouse_atck.tscn")
 
 func handle_rotation(angle:String) -> void :
 	if angle == 'R':
@@ -21,7 +22,6 @@ func handle_rotation(angle:String) -> void :
 		angle_cible += deg_to_rad(-45)
 
 func _anim():
-	
 	if (Input.is_action_pressed("down")):
 		animSprite.play("down")
 	elif(Input.is_action_pressed("up")):
@@ -31,9 +31,18 @@ func _anim():
 	elif(Input.is_action_pressed("right")):
 		animSprite.play("right")
 
+func ventouse_atck(mouse_pos):
+	var atck_instance = ventouse_preload.instantiate()
+	add_child(atck_instance)
+	atck_instance.position = position
+	if (mouse_pos.x > 500):
+		atck_instance.position = Vector3(0.300, -0.200, 0)
+	else:
+		atck_instance.position = Vector3(-0.300, -0.200, 0)
+
 func _process(delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
-	var ray_origin = camera.project_ray_origin(mouse_pos)    
+	var ray_origin = camera.project_ray_origin(mouse_pos)
 	var ray_dir = camera.project_ray_normal(mouse_pos)
 	var direction_face_camera = camera.global_transform.basis.z
 	var distance_mur: float = 5.0
@@ -45,16 +54,15 @@ func _process(delta: float) -> void:
 		torch_light.rotation.z = 0
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-#
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):# and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 	
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	
 	var direction:Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if (direction):
-		print("direcion : ", direction)
 		_anim()
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -71,28 +79,28 @@ func _process(delta: float) -> void:
 		CameraCoolDown.start()
 		handle_rotation('R')
 	rotation.y = lerp_angle(rotation.y, angle_cible, 10 * delta)
+	if (Input.is_action_pressed("leftClick")):
+		ventouse_atck(mouse_pos)
+	if (Input.is_action_just_pressed("h")):
+		$SpringArm3D/Camera3D/HelpMenu.visible = !$SpringArm3D/Camera3D/HelpMenu.visible
+	_torch_blink()
 	move_and_slide()
 
 func _on_camera_rotation_cooldown_timeout() -> void:
 	canrotate = true
 
-func _on_torch_blinking_timeout() -> void:
-	torch_count -= 1
-	if (torch_count == 0):
-		torch_blink = false
-		torch_count = 3
-		torch_light.light_energy = 1
-	else:
-		blinkTimer.start()
+func _torch_blink():
+	torch_count = randi()
+	if (torch_blink == true):
 		if (torch_count % 2 == 0):
 			torch_light.light_energy = 0
 		else:
 			torch_light.light_energy = 1
 
-func _torch_blink():
-		blinkTimer.start()
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
-		if (body.is_in_group("monstre") && torch_blink == false):
-			torch_blink = true
-			_torch_blink()
+	if (body.is_in_group("monstre") && torch_blink == false):
+		torch_blink = true
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if (body.is_in_group("monstre")):
+		torch_blink = false
