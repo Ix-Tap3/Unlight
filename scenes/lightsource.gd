98adb646d09d05fbf97276b65f4aeb7bfe11b96d
Mael Lebrun <malebrun@2F5.42angouleme.fr>
2026-05-24 19:17:43 +0200

    map

--- scenes/lightsource.gd
+++ scenes/lightsource.gd
@@ -4,19 +4,22 @@ extends Node3D
 @onready var light : SpotLight3D = $Light
 @onready var LitArea : Area3D = $LitArea
 @onready var PARTICULE : GPUParticles3D = $particule
-
+@onready var OMNI : OmniLight3D = $OmniLight3D
 func _ready() -> void:
 	if (has_ls):
 		light.light_energy = Var.lightPower
+		OMNI.light_energy = Var.lightPower
 	else :
 		light.light_energy = 0
+		OMNI.light_energy = 0
 	PARTICULE.emitting = false 
 
 func give_ls() -> void:
 	has_ls = true
 	light.light_energy = Var.lightPower
 	PARTICULE.emitting = true
-	
+	OMNI.light_energy = Var.lightPower
+
 	var myobjects = LitArea.get_overlapping_bodies()
 	for obj in myobjects:
 		if (obj.is_in_group("lightreactive")):
@@ -26,8 +29,8 @@ func give_ls() -> void:
 func take_ls() -> void:
 	has_ls = false
 	light.light_energy = 0
+	OMNI.light_energy = 0
 	PARTICULE.emitting = false
-	
 	var myobjects = LitArea.get_overlapping_bodies()
 	for obj in myobjects:
 		if (obj.is_in_group("lightreactive")):
