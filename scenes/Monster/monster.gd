9c90d03c78e3136cb369b3ecc188b5974ef2bf4e
Mael Lebrun <malebrun@2F5.42angouleme.fr>
2026-05-24 18:12:49 +0200

    los monstros

--- scenes/Monster/monster.gd
+++ scenes/Monster/monster.gd
@@ -1,30 +1,43 @@
 extends CharacterBody3D
 
+const SOLEIL_SCENE = preload("res://scenes/soleil_fragment.tscn")
+@onready var myarea: Area3D = $Area3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+# Cette variable passe à true dès que l'Area3D du joueur entre dans myarea
+var joueur_dans_la_zone: bool = false
 
 func _ready() -> void:
 	$Sprite3D.play("default")
+	# On connecte les signaux d'Area à Area
+	myarea.area_entered.connect(_on_area_entered)
+	myarea.area_exited.connect(_on_area_exited)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravité pour ton CharacterBody3D
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	# On écoute le clavier de manière ultra-précise ici
+	if self.visible and joueur_dans_la_zone:
+		if Input.is_action_just_pressed("kill"):
+			executer_remplacement()
+
+func _on_area_entered(area: Area3D) -> void:
+	# On vérifie si l'Area3D qui entre est celle du joueur (ou est dans le groupe)
+	if area.is_in_group("player") or area.get_parent().is_in_group("player"):
+		joueur_dans_la_zone = true
+
+func _on_area_exited(area: Area3D) -> void:
+	if area.is_in_group("player") or area.get_parent().is_in_group("player"):
+		joueur_dans_la_zone = false
+
+func executer_remplacement() -> void:
+	var instance = SOLEIL_SCENE.instantiate()
+	var position_actuelle = self.global_position
+	
+	get_parent().add_child(instance)
+	instance.global_position = position_actuelle + Vector3(0, 0.001, 0)
+	
+	queue_free()
--- scenes/soleilpickup.gd
+++ scenes/soleilpickup.gd
@@ -1,13 +1,28 @@
 extends Area3D
 
+# Cette variable se souvient si le joueur est dans la zone ou non
+var joueur_dans_la_zone: bool = false
+
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited) # On connecte aussi la sortie de zone
+
+func _process(delta: float) -> void:
+	# Le _process tourne en continu, il captera la touche "kill" à n'importe quel moment !
+	if joueur_dans_la_zone and Input.is_action_just_pressed("kill"):
+		_ramasser_item()
 
 func _on_area_entered(area) -> void:
-	print("test")
 	if area.is_in_group("player"):
-		_ramasser_item()
+		print("Le joueur est entré dans la zone")
+		joueur_dans_la_zone = true
+
+func _on_area_exited(area) -> void:
+	if area.is_in_group("player"):
+		print("Le joueur est sorti de la zone")
+		joueur_dans_la_zone = false
 
 func _ramasser_item() -> void:
 	Var.SolarFrag += 1
+	print("Item ramassé ! Score : ", Var.SolarFrag)
 	self.get_parent().queue_free()
